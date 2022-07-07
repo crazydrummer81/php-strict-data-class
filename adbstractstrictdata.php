c70641bcb0e8c8f3e2cbf307b9dea8eb6aa2023a
Mansur Mamirov <crazy_drummer@mail.ru>
2022-07-07 11:27:14 +0600

    AbstractStrictData class done

---
+++ adbstractstrictdata.php
@@ -0,0 +1,50 @@
+<?php
+class AbstractStrictData {
+  function __construct()
+  {
+    $this->inspectPropTypes();
+  }
+
+  private function inspectPropTypes()
+  {
+    $props = get_object_vars($this);
+
+    foreach ($props as $propName => $propValue) {
+      if (gettype($propValue) === 'NULL') {
+        throw new \InvalidArgumentException ('Property "'.$propName.'" can not be NULL in class "'.self::class.'" constructor. In might be initialized with some value.');
+      } 
+    }
+  }
+
+  public function __get($property)
+  {
+    if (property_exists($this, $property)) {
+      return $this->$property;
+    }
+    throw new \InvalidArgumentException('Class "'.self::class.'" does not have "'.$property.'" property');
+  }
+  
+  public function __set($property, $value)
+  {
+    if (!property_exists($this, $property)) {
+      throw new \InvalidArgumentException('Class "'.self::class.'" does not have "'.$property.'" property');
+    }
+
+    if (gettype($value) !== gettype($this->$property)) {
+      throw new \InvalidArgumentException('Object property "'.$property.'" must by type "'.gettype($this->$property).'", "'.gettype($value).'" given.');
+    }
+
+    $this->$property = $value;
+    return $this;
+  }
+
+  public function toArray()
+  {
+    $props = get_object_vars($this);
+    $array = [];
+    foreach ($props as $propName => $propValue) {
+      $array[$propName] = $propValue;
+    }
+    return $array;
+  }
+}
